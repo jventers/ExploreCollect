15f723b9fc3c73aed479f34fee10f07f79066a23
alex08dunn <alex08dunn@gmail.com>
2016-04-06 14:58:08 -0600

    added nearby search script and gnerate trip page for testing

--- scripts/nearbySearch.js
+++ scripts/nearbySearch.js
@@ -9,11 +9,7 @@ var searchResults;
 function initMap() {
     var pyrmont = {lat: -33.866, lng: 151.196};
 
-//            map = new google.maps.Map(document.getElementById('map'), {
-//                center: pyrmont,
-//                zoom: 17
-//            });
-    var map = new google.maps.Map(document.createElement('div'))
+     map = new google.maps.Map(document.createElement('div'))
 
     var service = new google.maps.places.PlacesService(map);
     service.nearbySearch({
@@ -48,31 +44,15 @@ function processResults(results, status, pagination) {
 }
 
 function createMarkers(places) {
-    var bounds = new google.maps.LatLngBounds();
 
     var placesList = document.getElementById('places');
 
     for (var i = 0, place; place = places[i]; i++) {
-        var image = {
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(25, 25)
-        };
-
-        var marker = new google.maps.Marker({
-            map: map,
-            icon: image,
-            title: place.name,
-            position: place.geometry.location
-        });
 
         placesList.innerHTML += '<li>' + place.name + '</li>';
 
-        //              bounds.extend(place.geometry.location);
     }
-//            map.fitBounds(bounds);
+
 }
 
 //<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBaWY5QH4ObetGu6TxKARoUpxWggHtJoiE&libraries=places&callback=initMap" async
